910f73ce3f7711d162fb929976a032d890cc9ae4
Fernanda Kipper <nanda.kipper@gmail.com>
2023-09-27 16:10:54 -0300

    feat: create form structure and style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormsComponent } from './components/forms/forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.scss']
+})
+export class FormsComponent implements OnInit {
+
+  results = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
